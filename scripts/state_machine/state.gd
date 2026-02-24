d6e9a528c1295f8994cf42c705d5424820b6ffad
Steven Truong <skolton09@gmail.com>
2026-02-23 19:25:01 -0700

    Created base state machine code

---
+++ scripts/state_machine/state.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name State
+
+signal state_transition
+
+func enter():
+	pass
+	
+func update(delta : float):
+	pass
+	
+func exit():
+	pass
---
+++ scripts/state_machine/state_machine.gd
@@ -0,0 +1,56 @@
+extends Node
+class_name StateMachine
+
+var states: Dictionary = {}
+var current_state: State
+@export var initial_state: State
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.state_transition.connect(change_state)
+			
+		if initial_state:
+			initial_state.enter()
+			current_state = initial_state
+
+func _process(delta):
+	if current_state:
+		current_state.update(delta)
+
+func force_change_state(new_state_name: String):
+	var new_state = states.get(new_state_name.to_lower())
+	
+	if !new_state:
+		print(new_state + " does not exist in the state machine")
+		return
+		
+	if current_state == new_state:
+		print("State is already set to that")
+	
+	if current_state:
+		var exit_callabe = Callable(current_state, "exit")
+		exit_callabe.call_deferred()
+		
+	new_state.enter()
+	
+	current_state = new_state
+
+func change_state(old_state : State, new_state_name: String):
+	if old_state != current_state:
+		print("Invalid change_state trying from: " + old_state.name + " but currently in: " + current_state.name)
+		return
+	
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		print(new_state + " does not exist in the state machine")
+		return
+		
+	if current_state:
+		var exit_callabe = Callable(current_state, "exit")
+		exit_callabe.call_deferred()
+		
+	new_state.enter()
+	
+	current_state = new_state
