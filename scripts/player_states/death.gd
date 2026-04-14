e71ab66f969be5440a9b6a83f1ffe493230dbec3
Thomas Urias <thomas.urias19@gmail.com>
2026-04-14 12:53:46 -0700

    Fixed bugs with health bar system when being damaged by vines

--- scripts/player_states/death.gd
+++ scripts/player_states/death.gd
@@ -2,6 +2,8 @@ extends State
 class_name PlayerDeath
 
 @onready var fade_anim_player: AnimationPlayer = get_tree().current_scene.get_node("CanvasLayer/AnimationPlayer")
+@onready var hearts_ui: CanvasLayer = get_tree().current_scene.get_node("CanvasLayer")
+
 var debounce = false
 
 func enter():
@@ -17,11 +19,13 @@ func update(delta):
 	await fade_anim_player.animation_finished
 
 	character.lives -= 1
+	hearts_ui.update_hearts(character.lives)
 	print("Player has lost a life")
 	print(character.lives)
 
 	if character.lives <= 0:
 		print("Calling game over scene change")
+		Checkpoint.checkpoint_pos = Checkpoint.inital_checkpoint
 		SceneTransition.change_scene("res://scenes/game_over.tscn")
 		return
 
